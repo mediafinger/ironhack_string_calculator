a1643a34bb9bfcb7e64b29db06cb700ce8ca6e32
Andreas Finger <webmaster@mediafinger.com>
2016-04-17 13:58:12 +0200

    Add more test cases for class method .add (red)

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,5 +7,19 @@ describe StringCalculator do
         expect(StringCalculator.add("")).to eql(0)
       end
     end
+
+    context "given a single number" do
+      context "given '4'" do
+        it "returns 4" do
+          expect(StringCalculator.add("4")).to eql(4)
+        end
+      end
+
+      context "given '10'" do
+        it "returns 10" do
+          expect(StringCalculator.add("10")).to eql(10)
+        end
+      end
+    end
   end
 end
