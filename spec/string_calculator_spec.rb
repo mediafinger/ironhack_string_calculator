0ab0502d02e25b60bfc9862a42dc62d1063baaaa
Andreas Finger <webmaster@mediafinger.com>
2016-04-17 14:14:40 +0200

    Add test cases for input with two numbers (red)

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,15 +9,29 @@ describe StringCalculator do
     end
 
     context "given a single number" do
-      context "given '4'" do
+      context "'4'" do
         it "returns 4" do
           expect(StringCalculator.add("4")).to eql(4)
         end
       end
 
-      context "given '10'" do
+      context "'10'" do
         it "returns 10" do
           expect(StringCalculator.add("10")).to eql(10)
+        end
+      end
+    end
+
+    context "given two numbers" do
+      context "'2,4'" do
+        it "returns 6" do
+          expect(StringCalculator.add("2,4")).to eql(6)
+        end
+      end
+
+      context "'17,100'" do
+        it "returns 117" do
+          expect(StringCalculator.add("17,100")).to eql(117)
         end
       end
     end
