0e33027c0e8570e883651f5bdca6907bdb33fc4d
Andreas Finger <webmaster@mediafinger.com>
2016-04-17 15:02:44 +0200

    .add returns floats unless decimals are all 0 (green)

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,12 @@
 class StringCalculator
   def self.add(string)
     numbers = string.split(",")
-    numbers.reduce(0) { |sum, number| sum + number.to_i }
+    result = numbers.reduce(0) { |sum, number| sum + number.to_f }
+    strip_insignificant_zeros(result)
   end
+
+  def self.strip_insignificant_zeros(number)
+    number.to_i == number ? number.to_i : number
+  end
+  private_class_method :strip_insignificant_zeros
 end
