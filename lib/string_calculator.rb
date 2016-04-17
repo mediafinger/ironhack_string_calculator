76842a1779ae3f5c1c63808d199b047eba5b1824
Andreas Finger <webmaster@mediafinger.com>
2016-04-17 14:42:30 +0200

    Refactor .add to handle multiple numbers (green)

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,6 @@
 class StringCalculator
   def self.add(string)
     numbers = string.split(",")
-    numbers[0].to_i + numbers[1].to_i
+    numbers.reduce(0) { |sum, number| sum + number.to_i }
   end
 end
